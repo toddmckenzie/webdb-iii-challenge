fc2fbe1d843f11b995b957dfce8c9ee5f539d7be
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-05 14:40:40 -0500

    added dependencies

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex')
+
+server.listen(4000, () => {
+    console.log('server is listening on 4000.')
+})
